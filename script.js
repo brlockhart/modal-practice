4ace113fdf3559954e9e38228bae217382c70056
Brandon L <bimk21@gmail.com>
2024-05-15 13:00:21 -0600

    complete modal practice

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+//open modal
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', showModal);
+}
+//close button
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+//esc to close modal
+document.addEventListener('keydown', function (e) {
+  console.log(`key pressed`, e.key);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
